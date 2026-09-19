8f723b933b564f7b6097a344ccceb05d0c702fc1
Hira-shi <carreljeremie@gmail.com>
2026-09-19 15:10:21 +0200

    Togglesplit clean

--- hypr/hyprconf/input.lua
+++ hypr/hyprconf/input.lua
@@ -4,28 +4,28 @@
 
 -- https://wiki.hypr.land/Configuring/Variables/#input
 hl.config({
-    input = {
-        kb_layout = "ch",
-        kb_variant = "fr",
-        kb_model = "",
-        kb_options = "",
-        kb_rules = "",
-        follow_mouse = 1,
-        sensitivity = 0,
-        touchpad = {
-            natural_scroll = false,
-        },
-    },
+	input = {
+		kb_layout = "us",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+		follow_mouse = 1,
+		sensitivity = 0,
+		touchpad = {
+			natural_scroll = false,
+		},
+	},
 })
 
 -- https://wiki.hypr.land/Configuring/Variables/#gestures
 hl.config({
-    gestures = {},
+	gestures = {},
 })
 
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Keywords/#per-device-input-configs for more
 hl.device({
-    name = "epic-mouse-v1",
-    sensitivity = -0.5,
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
 })
